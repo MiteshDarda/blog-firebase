ed075871b95b5cf7b84ccaed735fcc15db9cb0ba
MiteshDarda <mitesh.darda.0@gmai.com>
2022-03-04 18:47:39 +0530

    initialize react & tailwind & firebase

---
+++ src/components/App.js
@@ -0,0 +1,12 @@
+function App() {
+  return (
+    <div>
+      starting with blog 
+      <h1 className="text-3xl font-bold underline">
+      Hello world!
+    </h1>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/firebase.js
@@ -0,0 +1,15 @@
+import firebase from 'firebase/app'
+import 'firebase/firestore'
+import { initializeApp } from "firebase/app";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyAquiLyyKKDAFsO2J-Oil-BvzNmsPbT7S4",
+  authDomain: "blog-d4798.firebaseapp.com",
+  projectId: "blog-d4798",
+  storageBucket: "blog-d4798.appspot.com",
+  messagingSenderId: "239947324972",
+  appId: "1:239947324972:web:00de209c6284cdec75bb89"
+};
+const app = initializeApp(firebaseConfig);
+
+export const firestore = firebase.firestore()
