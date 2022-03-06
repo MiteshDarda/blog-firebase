436ec2e4352a7af46c62660f55c4474152e2acc9
MiteshDarda <mitesh.darda.0@gmai.com>
2022-03-06 23:54:38 +0530

    add-- home-page

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,8 +1,45 @@
+import { useEffect, useState } from "react";
+import { Link } from "react-router-dom";
+import { firestore } from "../firebase";
+
 export default function Home() {
+  const [posts, setPosts] =  useState([])
+
+  useEffect(() => {
+    firestore.collection('posts').get()
+      .then((snapshot) => {
+        const posts = snapshot.docs.map((doc) => {
+          return {
+            id: doc.id,
+            ...doc.data()
+          }
+        })
+        console.log(posts)
+        setPosts(posts)
+      })
+  },[])
+
   return (
-    <div className=''>
-      Home
-      this is home
+    <div className='p-2'>
+      <div className=" flex justify-evenly align-middle flex-wrap">
+        {posts.map((post, index) => {
+          return (
+            <div
+              className="
+              border-black border-2
+              border-solid h-32 p-2 bg-white m-4 min-w-[15rem]"
+            >
+              <Link to={`post/${post.id}`}>
+                <div
+                  className="flex-col justify-center align-middle"
+                >
+                  <p className=" text-2xl">{post.title}</p>
+                  <p className="text-lg">{ post.subTitle}</p>
+                </div>
+              </Link>
+            </div>
+          )})}
+      </div>
     </div>
   );
 }
--- src/components/PostDetail.js
+++ src/components/PostDetail.js
@@ -1,7 +1,17 @@
 export default function PostDeatil() {
   return (
-    <div className=''>
-        PostDeatil
+    <div className='p-2 flex justify-center align-middle'>
+      <div className=" flex-col justify-center align-middle">
+        <h1 className=" font-extrabold text-5xl">Title</h1>
+        <br/>
+        <br/>
+        <h1 className=" font-bold text-3xl">sub-title</h1>
+        <br/>
+        <br/>
+        <br/>
+        <br />
+        <p>para</p>
+      </div>
     </div>
   );
 }
